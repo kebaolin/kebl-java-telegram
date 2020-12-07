e382c61c72837b170263b9ae07dceebbd4a2b45a
yehongjuan <kebl@asiainfo.com>
2020-12-07 16:15:53 +0800

    数组学习
    2020-12-07

---
+++ local-test/src/javademo/base/TestArrays.java
@@ -0,0 +1,37 @@
+package javademo.base;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestArrays {
+	public static void main(String[] args) {
+//		int[] ns = {1, 2, 3, 4, 5}; 
+//		int[] ns1 = {};
+//		Set<String[][]> dejaVu = new HashSet<String[][]>();
+//		String[][] ns2 ={ {"xyz", "abc", "def", "ggg"}, {"hhb", "hjn", "mkl"}, {"dcv", "wds", "qsa"}};
+//		String[] ns3 = {"xyz", "abc", "def", "ggg"};
+//		System.out.println(Arrays.toString(ns));
+//		System.out.println(Arrays.toString(ns1));
+//		System.out.println(Arrays.deepToString(ns2));
+//		System.out.println(Arrays.deepToString(ns3));
+//		System.out.println(ns2.getClass());
+//		System.out.println(Object[].class);
+		int[][] scores = {{87,98,98}, {97,76,87}, {98,98,99},{100,95,96,86}};
+		int i = 0;
+		double sum =0;
+		double avg;
+		for (int[] sr : scores) {
+			for (int score : sr) {
+				sum += score;
+				i++;
+			}
+		}
+		System.out.println(i);
+		System.out.println(sum);
+		avg = sum /i;
+		System.out.println(avg);
+		System.out.printf("学生的平均分为: %.2f", avg);
+	}
+
+}
